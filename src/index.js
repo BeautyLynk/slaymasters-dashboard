cefda2ed9bf58e126e98eebed5dc030ffcc5f649
Abdou <sene78@hotmail.com>
2022-05-23 13:36:30 -0400

    1.4

--- src/index.js
+++ src/index.js
@@ -12,26 +12,22 @@ import UploadImages from './pages/UploadImages'
 import { Helmet } from "react-helmet";
 
 
-ReactDOM.render(
-  <AuthProvider>
-    {/* <Helmet>
-      <head> 
-          <title>SLAYMASTER - Create Beauty Content That Slays, Hair Tutorial</title>
-          <meta 
-              name="description" 
-              content="Build an empowering community for #EVERYONES beauty journey through video, blogging, and other forms of media. CREATE BEAUTY CONTENT THAT SLAYS!"
-              data-react-helmet="true" 
-          />
-      </head>  
-    </Helmet> */}
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home/>}/>
-        <Route path={"sign-in"} element={<SignIn/>}/>
-        <Route path={"dashboard"} element={<ProtectedRoute> <Dashboard/> </ProtectedRoute>}/>
-        <Route path={"upload-images"} element={<ProtectedRoute> <UploadImages/> </ProtectedRoute>}/>
-      </Routes>
-    </BrowserRouter>
-  </AuthProvider>,
-  document.getElementById('root')
-);
+import { hydrate, render } from "react-dom";
+
+const APP = (<AuthProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<Home/>}/>
+      <Route path={"sign-in"} element={<SignIn/>}/>
+      <Route path={"dashboard"} element={<ProtectedRoute> <Dashboard/> </ProtectedRoute>}/>
+      <Route path={"upload-images"} element={<ProtectedRoute> <UploadImages/> </ProtectedRoute>}/>
+    </Routes>
+  </BrowserRouter>
+</AuthProvider>)
+ 
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(APP, rootElement);
+} else {
+  render(APP, rootElement);
+}
