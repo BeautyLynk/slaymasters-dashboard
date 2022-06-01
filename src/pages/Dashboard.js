7f37f46be585370e24fe3649a91ca031868832be
Abdou <sene78@hotmail.com>
2022-06-01 14:23:07 -0400

    1.6

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -279,14 +279,15 @@ const Dashboard = () => {
 
                     <div className="tutorial-iframe-container">
                         <h1>How to Use the Dashboard</h1>
-                        <div style="position: relative; padding-bottom: 66.66666666666666%; height: 0;">
+                        <div className="how_to_use_dash">
                             <iframe 
                                 src="https://www.loom.com/embed/6d4a401469a349a29b80ed4611080c20" 
                                 frameborder="0" 
                                 webkitallowfullscreen 
                                 mozallowfullscreen 
                                 allowfullscreen 
-                                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">
+                                className="how_to_use_dash-video"
+                            >
                             </iframe>
                         </div>
                     </div>
