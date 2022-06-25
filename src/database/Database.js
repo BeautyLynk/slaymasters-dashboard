962c519aecb54779ad51b0f166d60a54db8d8b5d
Abdou <sene78@hotmail.com>
2022-06-25 13:22:13 -0400

    1.7

--- src/database/Database.js
+++ src/database/Database.js
@@ -9,8 +9,15 @@ import { createClient } from '@supabase/supabase-js'
 
 // const supabaseUrl = 'https://sduxzxrctlxqqbmtfsqr.supabase.co'
 // const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkdXh6eHJjdGx4cXFibXRmc3FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDg3Njg4ODUsImV4cCI6MTk2NDM0NDg4NX0.Pn4iPwRlfN0apCKdPhWjD6N5c20lNrEy8FSdJL_FdWE'
-const supabaseKey = process.env.REACT_APP_SUPABASE_PUBLIC_KEY
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+// const supabaseKey = process.env.REACT_APP_SUPABASE_PUBLIC_KEY
+// const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 
-// Export the variable to be used in other files
-export const supabase = createClient(supabaseUrl, supabaseKey)
+// // Export the variable to be used in other files
+// export const supabase = createClient(supabaseUrl, supabaseKey)
+
+const supabase = createClient(
+    process.env.REACT_APP_SUPABASE_URL,
+    process.env.REACT_APP_SUPABASE_PUBLIC_KEY,
+)
+
+export { supabase }
